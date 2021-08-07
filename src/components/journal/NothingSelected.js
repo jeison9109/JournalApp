875e860db63e4afba6492e4705f28bd30348c8a6
Yeison Serna <yeisonivanserna@gmail.com>
2021-08-06 23:45:54 -0500

    se agrego cuadro de notas

---
+++ src/components/journal/NothingSelected.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const NothingSelected = () => {
+  return (
+    <div className="nothing__main-content">
+      <p>Selecciona algo</p>
+      Crea un evento
+      <br />
+      <i className="far fa-star fa-4x mt-5" />
+    </div>
+  );
+};
---
+++ src/components/notes/NotesAppBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const NotesAppBar = () => {
+  return (
+    <div className="notes__appbar">
+      <span>6 Agosto 2021</span>
+      <div>
+        <button className="btn">Imagen</button>
+      </div>
+
+      <div>
+        <button className="btn">Guardar</button>
+      </div>
+    </div>
+  );
+};
