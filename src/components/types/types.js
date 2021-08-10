b03a0e385f605ecd2c62f1aa21f317b4e47502cf
Yeison Serna <yeisonivanserna@gmail.com>
2021-08-10 11:56:29 -0500

    Types para autentiacion manual y google

---
+++ src/components/types/types.js
@@ -0,0 +1,10 @@
+export const types = {
+  login: "[Auth] Login",
+  logout: "[Auth] Logout",
+
+  uiSetError: "[UI] setError",
+  uiRemoveError: "[UI] removeError",
+
+  uiStartLoading: "[UI] Start loading",
+  uiFinishLoading: "[UI] Finish loading",
+};
