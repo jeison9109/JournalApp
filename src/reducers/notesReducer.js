a7be52b69a4ca7624619fa60e08793886cec65d7
Yeison Serna <yeisonivanserna@gmail.com>
2021-08-12 13:11:30 -0500

    reducers load - update notas firebase

---
+++ src/reducers/notesReducer.js
@@ -0,0 +1,35 @@
+import { types } from "../components/types/types";
+
+const initialState = {
+  note: [],
+  active: null,
+};
+
+export const notesReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case types.notesActive:
+      return {
+        ...state,
+        active: {
+          ...action.payload,
+        },
+      };
+
+    case types.notesLoad:
+      return {
+        ...state,
+        notes: [...action.payload],
+      };
+
+    case types.notesUpdate:
+      return {
+        ...state,
+        notes: state.notes.map((note) =>
+          note.id === action.payload.id ? action.payload.note : note
+        ),
+      };
+
+    default:
+      return state;
+  }
+};
